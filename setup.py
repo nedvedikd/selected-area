94f4ee5f1a4bf555fcedc07aabf7ac90071191ff
Dominik Nedvedik <dominik.nedvedik@protonmail.com>
2020-06-17 08:27:00 +0200

    fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="selected-area",  # Replace with your own username
-    version="0.1.0",
+    version="0.1.1",
     author="Dominik Nedvedik",
     author_email="dominik.nedvedik@protonmail.com",
     description="Calculates if plot/segment intersects the selected area.",
